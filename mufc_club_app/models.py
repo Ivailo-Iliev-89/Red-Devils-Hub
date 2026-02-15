a1b7c1034592721c55b8848514da776153f30f34
Mihail Markovski <misho5071@gmail.com>
2026-02-15 22:50:22 +0200

    Setup Player model,  refactor index.html JavaScript to a  JS fetch for real-time loading  and update the navbar  with anchor links

--- mufc_club_app/models.py
+++ mufc_club_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name')
+    position = models.CharField(max_length=50, verbose_name='Position')
+    number = models.PositiveBigIntegerField(verbose_name='Number')
+    nationality = models.CharField(max_length=50, verbose_name='Nationality')
+
+    class Meta:
+        verbose_name = 'Player'
+        verbose_name_plural = 'Players'
+
+    def __str__(self):
+        return f"{self.number}. {self.name}"
--- mufc_club_app/views.py
+++ mufc_club_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Player
+from .serializers import PlayerSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def learn_more(request):
+    return render(request, 'learn-more.html')
+
+
+class PlayerListAPI(generics.ListAPIView):
+    queryset = Player.objects.all().order_by('number')
+    serializer_class = PlayerSerializer
